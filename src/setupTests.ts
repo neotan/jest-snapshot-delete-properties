55801428c791dcd3b710b268b566775cbb12efa5
Neo Tan <neotan12@hotmail.com>
2026-04-04 01:22:44 -0400

    chore: update Jest config for Jest 29, remove Enzyme setup

--- src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable:no-implicit-dependencies */
-import "jest";
-import "raf/polyfill";
-
-import {configure} from "enzyme";
-import * as Adapter from "enzyme-adapter-react-16";
-
-configure({adapter: new Adapter()});
